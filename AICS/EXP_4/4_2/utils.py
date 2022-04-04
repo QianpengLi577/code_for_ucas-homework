e11a69ebe443ce2bbfcfd6d26d08624c70180722
QianpengLi577 <55045729+QianpengLi577@users.noreply.github.com>
2022-04-04 11:47:53 +0800

    Update utils.py

--- AICS/EXP_4/4_2/utils.py
+++ AICS/EXP_4/4_2/utils.py
@@ -1,5 +1,6 @@
-import scipy.misc, numpy as np, os, sys
 # coding=utf-8
+import scipy.misc, numpy as np, os, sys
+
 def save_img(out_path, img):
     img = np.clip(img, 0, 255).astype(np.uint8)
     scipy.misc.imsave(out_path, img)
